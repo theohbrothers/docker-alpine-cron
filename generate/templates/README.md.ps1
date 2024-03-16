641c1e157aa9e4a656df6c30a80547d0a2eed496
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-03-16 09:18:12 +0000

    Docs (readme): Update GitHub CI badge
    Signed-off-by: Leonard Jonathan Oh <leonardjonathanoh@live.com>

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-alpine-cron
 
-[![github-actions](https://github.com/theohbrothers/docker-alpine-cron/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-alpine-cron/actions)
+[![github-actions](https://github.com/theohbrothers/docker-alpine-cron/actions/workflows/ci-master-pr.yml/badge.svg?branch=master)](https://github.com/theohbrothers/docker-alpine-cron/actions/workflows/ci-master-pr.yml)
 [![github-release](https://img.shields.io/github/v/release/theohbrothers/docker-alpine-cron?style=flat-square)](https://github.com/theohbrothers/docker-alpine-cron/releases/)
 [![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-alpine-cron/latest)](https://hub.docker.com/r/theohbrothers/docker-alpine-cron)
 
